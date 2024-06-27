de9f336bdf942a2b27440bbbc55d4a45d6a5ec23
Alex Gerasimov <alcordom@gmail.com>
2024-06-27 19:48:57 +0400

    fixed a bug with a border

--- src/components/ui/fields/Field.tsx
+++ src/components/ui/fields/Field.tsx
@@ -53,7 +53,7 @@ export const Field = forwardRef<HTMLInputElement, InputFieldProps>(
 					{label}
 				</label>
 				<input
-					className={`mt-2 flex w-full items-center justify-center rounded-lg border border-border bg-white/0 p-3 text-base outline-none placeholder:text-gray-700 placeholder:font-normal duration-500 transition-colors hover:border-secondary focus:border-primary ${
+					className={`mt-2 flex w-full items-center justify-center rounded-lg border border-primary bg-white/0 p-3 text-base outline-none placeholder:text-gray-700 placeholder:font-normal duration-500 transition-colors hover:border-secondary focus:border-primary ${
 						disabled
 							? '!border-none !bg-gray-100 dark:!bg-white/5 dark:placeholder:!text-[rgba(255,255,255,0.15)]'
 							: state === 'error'
