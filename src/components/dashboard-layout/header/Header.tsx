e1f2a67162f468d80d0655021960236f875efbb8
Alex Gerasimov <alcordom@gmail.com>
2024-07-01 05:41:24 +0400

    fixed loader

--- src/components/dashboard-layout/header/Header.tsx
+++ src/components/dashboard-layout/header/Header.tsx
@@ -2,11 +2,11 @@
 
 import Link from 'next/link'
 
-import { Loader } from '@/components/ui/Loader'
-
 import { DASHBOARD_PAGES } from '@/config/pages-url.config'
 
 import { useProfile } from '@/hooks/useProfile'
+
+import GlobalLoader from './GlobalLoader'
 
 export default function Header() {
 	const { data, isLoading } = useProfile()
@@ -14,7 +14,7 @@ export default function Header() {
 	return (
 		<header className='h-23 flex justify-between items-center p-big-layout bg-white shadow-md'>
 			{isLoading ? (
-				<Loader />
+				<GlobalLoader />
 			) : (
 				<>
 					<h1 className='text-3xl font-medium'>{`Hello, ${data?.user.name}!`}</h1>
--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -3,7 +3,7 @@ import { Loader as LoaderIcon } from 'lucide-react'
 export function Loader() {
 	return (
 		<div className='flex justify-center items-center'>
-			<LoaderIcon className='animate-spin h-5 w-5 text-white' />
+			<LoaderIcon className='animate-spin text-primary' />
 		</div>
 	)
 }
