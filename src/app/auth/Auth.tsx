7ab518d105d63e9f1bdc027bd3eb4b43698911bc
Alex Gerasimov <alcordom@gmail.com>
2024-07-06 21:59:33 +0400

    amended the auth stuff

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import { useMutation } from '@tanstack/react-query'
-import type { AxiosError } from 'axios'
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { type SubmitHandler, useForm } from 'react-hook-form'
-import { toast } from 'sonner'
 
+import { Loader } from '@/components/ui/Loader'
 import { Logo } from '@/components/ui/Logo'
 import { Button } from '@/components/ui/buttons/Button'
 import { Field } from '@/components/ui/fields/Field'
@@ -15,37 +13,19 @@ import type { IAuthForm } from '@/types/auth.types'
 
 import { DASHBOARD_PAGES } from '@/config/pages-url.config'
 
-import { authService } from '@/services/auth.service'
-
-interface AxiosErrorData {
-	readonly error: string
-	readonly message: string
-	readonly statusCode: number
-}
+import { TypeForm, useAuth } from './hooks/useAuth'
 
 export function Auth() {
-	const [isLoginForm, setIsLoginForm] = useState(false)
-	const { push, prefetch } = useRouter()
+	const { prefetch } = useRouter()
 	const { register, handleSubmit, reset } = useForm<IAuthForm>({
 		mode: 'onChange'
 	})
-	const { mutate } = useMutation({
-		mutationKey: ['auth'],
-		mutationFn: (data: IAuthForm) =>
-			authService.main(isLoginForm ? 'login' : 'register', data),
-		onSuccess() {
-			toast.success('Succesfully login!')
-			reset()
-			push(DASHBOARD_PAGES.HOME)
-		},
-		onError(err: AxiosError<AxiosErrorData>) {
-			toast.error(`An error has occured: ${err.response?.data.message}`)
-		}
-	})
+	const { isAuthPending, mutate, setTypeForm } = useAuth(reset)
 
 	useEffect(() => prefetch(DASHBOARD_PAGES.HOME), [prefetch])
 
 	const onSubmit: SubmitHandler<IAuthForm> = data => mutate(data)
+	const handleActionForm = (typeForm: TypeForm) => () => setTypeForm(typeForm)
 
 	return (
 		<div className='relative flex flex-col items-center justify-center  min-h-screen'>
@@ -57,33 +37,41 @@ export function Auth() {
 			<p>Login into your account</p>
 			<p>or register</p>
 
-			<form
-				className='w-1/3 p-layout'
-				onSubmit={handleSubmit(onSubmit)}
-			>
-				<Field
-					id='email'
-					extra='mb-4'
-					label='Email:'
-					placeholder='Type the email here...'
-					type='email'
-					{...register('email', { required: 'Email is required!' })}
-				/>
-
-				<Field
-					id='password'
-					extra='mb-6'
-					label='Password:'
-					placeholder='Type the password here...'
-					type='password'
-					{...register('password', { required: 'Password is required!' })}
-				/>
-
-				<div className='flex items-center justify-center gap-7'>
-					<Button onClick={() => setIsLoginForm(true)}>Login</Button>
-					<Button onClick={() => setIsLoginForm(false)}>Register</Button>
+			{isAuthPending ? (
+				<div className='flex items-center justify-center w-1/3 h-80'>
+					<Loader />
 				</div>
-			</form>
+			) : (
+				<form
+					className='w-1/3 min-h-80 p-layout'
+					onSubmit={handleSubmit(onSubmit)}
+				>
+					<Field
+						id='email'
+						extra='mb-4'
+						label='Email:'
+						placeholder='Type the email here...'
+						type='email'
+						{...register('email', { required: 'Email is required!' })}
+					/>
+
+					<Field
+						id='password'
+						extra='mb-6'
+						label='Password:'
+						placeholder='Type the password here...'
+						type='password'
+						{...register('password', { required: 'Password is required!' })}
+					/>
+
+					<div className='flex items-center justify-center gap-7'>
+						<Button onClick={handleActionForm(TypeForm.login)}>Login</Button>
+						<Button onClick={handleActionForm(TypeForm.register)}>
+							Register
+						</Button>
+					</div>
+				</form>
+			)}
 		</div>
 	)
 }
---
+++ src/app/auth/hooks/useAuth.ts
@@ -0,0 +1,42 @@
+import { useMutation } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { UseFormReset } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import type { IAuthForm } from '@/types/auth.types'
+
+import { DASHBOARD_PAGES } from '@/config/pages-url.config'
+
+import { authService } from '@/services/auth.service'
+
+interface AxiosErrorData {
+	readonly error: string
+	readonly message: string
+	readonly statusCode: number
+}
+
+export enum TypeForm {
+	login = 'login',
+	register = 'register'
+}
+
+export const useAuth = (reset: UseFormReset<IAuthForm>) => {
+	const { push } = useRouter()
+	const [typeForm, setTypeForm] = useState<TypeForm>(TypeForm.login)
+
+	const { isPending: isAuthPending, mutate } = useMutation({
+		mutationKey: ['auth'],
+		mutationFn: (data: IAuthForm) => authService.main(typeForm, data),
+		onSuccess() {
+			toast.success('Succesfully login!')
+			reset()
+			push(DASHBOARD_PAGES.HOME)
+		},
+		onError(err: AxiosError<AxiosErrorData>) {
+			toast.error(`An error has occured: ${err.response?.data.message}`)
+		}
+	})
+	return { isAuthPending, mutate, setTypeForm }
+}
